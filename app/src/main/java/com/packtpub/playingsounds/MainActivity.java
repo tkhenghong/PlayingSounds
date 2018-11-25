c4465427bbe04c0eeaa8dd6d0620eba17c816388
Teoh Kheng Hong <tkhenghong@gmail.com>
2018-11-25 18:44:40 +0800

    Added Content

--- app/src/main/java/com/packtpub/playingsounds/MainActivity.java
+++ app/src/main/java/com/packtpub/playingsounds/MainActivity.java
@@ -1,22 +1,32 @@
 package com.packtpub.playingsounds;
 
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private SoundPool soundPool; // For playing music
+    int sample1 = -1;
+    int sample2 = -1;
+    int sample3 = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +36,37 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        try {
+            //Create objects of the 2 required classes
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
+            //create our three fx in memory ready for use
+            descriptor = assetManager.openFd("sample1.ogg");
+            sample1 = soundPool.load(descriptor, 0);
+            descriptor = assetManager.openFd("sample2.ogg");
+            sample2 = soundPool.load(descriptor, 0);
+            descriptor = assetManager.openFd("sample3.ogg");
+            sample3 = soundPool.load(descriptor, 0);
+        } catch (IOException e) {
+            //catch exceptions here
+            System.out.println("Hey the app failed????");
+            e.printStackTrace();
+        }
+
+        //Make a button from each of the buttons in our layout
+        Button button1 = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        //Make each of them listen for clicks
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -48,5 +89,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button://when the first button is pressed
+                //Play sample 1
+                soundPool.play(sample1, 1, 1, 0, 0, 1);
+                break;
+                //Now the other buttons
+            case R.id.button2:
+                soundPool.play(sample2, 1, 1, 0, 0, 1);
+                break;
+            case R.id.button3:
+                soundPool.play(sample3, 1, 1, 0, 0, 1);
+                break;
+        }
     }
 }
